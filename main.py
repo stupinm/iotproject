30a7f395b5131898b6e9724f467c7b8c05fc0f04
stupinm <72613733+stupinm@users.noreply.github.com>
2020-10-09 17:59:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 from flask import (
-    Flask,
     render_template as render,
     request, 
     redirect, 
     send_from_directory
 )
+from tinydb import TinyDB, Query
+from dashboard import server as app
+import os
+import pathlib
 
-app = Flask(__name__)
+session = {'username': ''}
+
+app_path = str(pathlib.Path(__file__).parent.resolve())
+db_path = os.path.join(app_path, os.path.join("data", "db.json"))
+
+db = TinyDB(db_path, sort_keys=True, indent=4, separators=(',', ': '))
+usr = db.table('users')
 
 
 @app.errorhandler(404)
@@ -28,6 +37,44 @@ def send_data(path):
 @app.route('/signin', methods=['GET'])
 def signin():
     return render('signin.html')
+
+
+@app.route('/signup', methods=['GET'])
+def signup():
+    return render('signup.html')
+
+
+@app.route('/signout', methods=['GET'])
+def signout():
+    return redirect('/')
+
+
+@app.route('/signin', methods=['POST'])
+def do_signin():
+    User = Query()
+    users = usr.search(User.name == request.form['username'])
+    if not users:
+        return render('signin.html', text='Wrong username or password')
+    user = users[0]
+    if user['password'] != request.form['password']:
+        print(user['password'], request.form['password'])
+    session['username'] = user['name']
+    return redirect('dashboard')
+
+
+@app.route('/signup', methods=['POST'])
+def do_signup():
+    User = Query()
+    users = db.search(User.name == request.form['username'])
+    if len(users) > 0:
+        text = 'Such user have already exists'
+        return render('signup.html', text=text)
+    usr.insert({
+        'name' : request.form['username'],
+        'email': request.form['email'],
+        'password': request.form['password']
+    })
+    return redirect('/')
 
 
 if __name__ == '__main__':
